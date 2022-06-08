9d3bb29c71c35a55fa80524cc322dd600f01abcc
Fabricio Bencomo <Fabriciobencomo.viloria@gmail.com>
2022-06-07 23:05:23 -0400

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="container">
+      <div class="flex justify-around">
+          <div class="text-lg capitalize">fabricio bencomo</div>
+          <div>
+              <a href="#" class="text-selected-text">Home</a>
+              <a href="#about">About</a>
+              <a href="#work">Projects</a>
+              <a href="#hire"><button>Contact Me</button></a>
+          </div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
