d392de67537e617f4d87de92f8352ac67cbf1d08
Fabricio Bencomo <Fabriciobencomo.viloria@gmail.com>
2022-06-08 00:30:47 -0400

    header section added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,15 @@
 <template>
-  <header class="container">
-      <div class="flex justify-around">
+  <header class="py-6">
+      <div class="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
           <div class="text-lg capitalize">fabricio bencomo</div>
-          <div>
+          <div class="md:flex hidden space-x-12 items-center">
               <a href="#" class="text-selected-text">Home</a>
               <a href="#about">About</a>
               <a href="#work">Projects</a>
-              <a href="#hire"><button>Contact Me</button></a>
+              <a href="#hire"><button class="px-6 py-2 bg-theme font-bold">Contact Me</button></a>
+          </div>
+          <div class="md:hidden">
+              <img src="@/assets/responsive.svg" alt="menu">
           </div>
       </div>
   </header>
